087ad360147f8a525c1f76573128af58369e4d4f
vincent-the-engineer <208263664+vincent-the-engineer@users.noreply.github.com>
2026-03-03 23:17:22 +1100

    feat: Ch2 L4 Connect

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Peril server connected to RabbitMQ");
+  process.on("SIGINT", async () => {
+    try{
+      await conn.close();
+      console.log("Peril server closed connection to RabbitMQ");
+    } catch (err) {
+      console.log("Error closing RabbitMQ connection: ", err);
+    } finally {
+      process.exit(0);
+    }
+  });
 }
 
 main().catch((err) => {
